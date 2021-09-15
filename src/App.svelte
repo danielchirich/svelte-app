a5f2ab9d4f8efd08c426b286d144980a9907060b
danielchirich <daniel.chirich@gmail.com>
2021-09-15 18:56:37 -0300

    Update App.svelte
    actualizar app

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import Header from './Header.svelte';
-
+  import Troquelado from './Troquelado.svelte';
+  import Bandeja from './Bandeja.svelte';
   import Footer from './Footer.svelte';
-	import Clientes from './Clientes.svelte';
-	import Standart from './Standart.svelte';
-	import Producto from './Producto.svelte'; 
+  import Clientes from './Clientes.svelte';
+  import Standart from './Standart.svelte';
+  import Producto from './Producto.svelte'; 
   import Delete from "./Delete.svelte"; 
   import Upsert from './Upsert.svelte'; 
   
@@ -15,6 +16,8 @@
   let opcion3 = false
   let opcion4 = false
   let opcion5 = false
+  let opcion6 = false
+  let opcion7 = false
   
 </script>
 <Header />
@@ -124,6 +127,45 @@
            </label>
       </div>
 
+        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Cargar datos para Troquelado</h2> 
+           <div class="flex items-start justify-item-end w-full mb-12">
+  
+            <label for="toggleB6" class="flex items-center cursor-pointer">
+                  <!-- toggle -->
+                  <div class="relative">
+                   <!-- input -->
+                  <input type="checkbox" id="toggleB6" class="sr-only" bind:checked={opcion6}>
+                  <!-- line -->
+                  <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                  <!-- dot -->
+                  <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                  </div>
+                   <!-- label -->
+                  <div class="ml-3 text-gray-700 font-medium">
+                  Troquelado
+                   </div>
+           </label>
+        </div>
+
+        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Cargar datos para Bandeja</h2> 
+           <div class="flex items-start justify-item-end w-full mb-12">
+  
+            <label for="toggleB7" class="flex items-center cursor-pointer">
+                  <!-- toggle -->
+                  <div class="relative">
+                   <!-- input -->
+                  <input type="checkbox" id="toggleB7" class="sr-only" bind:checked={opcion7}>
+                  <!-- line -->
+                  <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                  <!-- dot -->
+                  <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                  </div>
+                   <!-- label -->
+                  <div class="ml-3 text-gray-700 font-medium">
+                  Troquelado
+                   </div>
+           </label>
+        </div>
   </div>
 </div>
 </section> 
@@ -145,6 +187,14 @@
 
 {#if opcion5}
           <Clientes/>
+{/if}
+
+{#if opcion6}
+          <Troquelado/>
+{/if}
+
+{#if opcion7}
+          <Bandeja/>
 {/if}
 </main>
 
