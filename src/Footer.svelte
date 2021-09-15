1934ba08475e047c9d1621e348a9e89fbbfc38d1
danielchirich <daniel.chirich@gmail.com>
2021-09-15 16:19:59 -0300

    agregar Header & Footer

---
+++ src/Footer.svelte
@@ -0,0 +1,41 @@
+<script>
+    import './lib/TailwindCSS.svelte'// your script goes here
+    
+</script>
+
+<footer class="text-gray-600 bg-gray-600 body-font">
+  <div class="container px-4 py-6 mx-auto flex  items-center sm:flex-row flex-col">
+    <a class=" flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 20 20">
+        <path fill="none" d="M12.739,9.059L12.739,9.059L12.739,9.059L12.739,9.059L12.739,9.059z M7.231,9.094l-1.962,1.587l1.706,1.025h0l0.372,0.225l0.75,0.45l1.255-0.953l0.629-0.481l0.002,0.002l0.003-0.002l0.011,0.008l0.006-0.008l1.494,1.112l0.437,0.321l2.798-1.7l-1.993-1.621l-2.75,1.743L7.231,9.094z M11.898,13.419l-0.006,0.006l-1.892-1.408l-1.814,1.389l-0.018,0.026l-1-0.604v0.527l2.83,1.81l2.807-1.81v-0.491L11.898,13.419L11.898,13.419z M5.266,7.569l1.97,1.522l2.751-1.742l-1.89-1.653L5.266,7.569zM11.983,5.695L9.987,7.349l2.752,1.709l1.995-1.577L11.983,5.695z M10,0.531c-5.229,0-9.469,4.239-9.469,9.469S4.771,19.469,10,19.469s9.469-4.239,9.469-9.469S15.229,0.531,10,0.531 M10,18.608c-4.754,0-8.608-3.854-8.608-8.608S5.246,1.392,10,1.392c4.754,0,8.608,3.854,8.608,8.608S14.754,18.608,10,18.608"></path>
+      </svg>
+      <span class="ml-3 text-xl">Corrugado app</span>
+    </a>
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2021 — danielchirich     
+    </p>
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a class="text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
---
+++ src/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+    import './lib/TailwindCSS.svelte'// your script goes here
+    
+</script>
+<header class="text-gray-600 bg-gray-600 body-font">
+<div class="  container px-4 py-6 mx-auto flex items-center sm:flex-row flex-col">
+    <a class=" flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 20 20">
+        <path fill="none" d="M12.739,9.059L12.739,9.059L12.739,9.059L12.739,9.059L12.739,9.059z M7.231,9.094l-1.962,1.587l1.706,1.025h0l0.372,0.225l0.75,0.45l1.255-0.953l0.629-0.481l0.002,0.002l0.003-0.002l0.011,0.008l0.006-0.008l1.494,1.112l0.437,0.321l2.798-1.7l-1.993-1.621l-2.75,1.743L7.231,9.094z M11.898,13.419l-0.006,0.006l-1.892-1.408l-1.814,1.389l-0.018,0.026l-1-0.604v0.527l2.83,1.81l2.807-1.81v-0.491L11.898,13.419L11.898,13.419z M5.266,7.569l1.97,1.522l2.751-1.742l-1.89-1.653L5.266,7.569zM11.983,5.695L9.987,7.349l2.752,1.709l1.995-1.577L11.983,5.695z M10,0.531c-5.229,0-9.469,4.239-9.469,9.469S4.771,19.469,10,19.469s9.469-4.239,9.469-9.469S15.229,0.531,10,0.531 M10,18.608c-4.754,0-8.608-3.854-8.608-8.608S5.246,1.392,10,1.392c4.754,0,8.608,3.854,8.608,8.608S14.754,18.608,10,18.608"></path>
+      </svg>
+  <span class="text-gray-400 ml-3 text-xl">Corrugado</span>
+    </a>
+    <p class="text-sm text-gray-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Aplicación Supabase con los productos de corrugado activos: cajas standart, troqueladas, etc.    
+    </p>    
+      
+      
+  
+</div>
+</header>
